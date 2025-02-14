841c58e67a589aad9dfa2e449a873013844f872c
suraphon <suraphon.k@merudy.com>
2025-02-14 16:59:57 +0700

    navber,footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,69 @@
+import { useState } from 'react'
+
 export default function Navbar() {
+   const [isOpen, setIsOpen] = useState(false)
+
    return (
-      <div>Navbar</div>
+      <>
+         <nav className="bg-blue-600 text-white bg-gradient-to-r from-blue-600 to-blue-400">
+            <div className="container mx-auto flex items-center justify-between p-4">
+               <a href="/" className="text-2xl font-bold hover:text-gray-200">
+                  Logo
+               </a>
+               {/* Menu Desktop */}
+               <ul className="hidden md:flex space-x-4">
+                  <li>
+                     <a href="#" className="hover:text-gray-200">
+                        Home
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" className="hover:text-gray-200">
+                        About
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" className="hover:text-gray-200">
+                        Service
+                     </a>
+                  </li>
+                  <li>
+                     <a href="#" className="hover:text-gray-200">
+                        Contact
+                     </a>
+                  </li>
+               </ul>
+               {/* Button Hamburger */}
+               <button className="md:hidden cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                  </svg>
+               </button>
+            </div>
+            {/* Menu Mobile */}
+            <ul className={`md:hidden flex-col space-y-4 p-4 ${isOpen ? 'block' : 'hidden'} `}>
+               <li>
+                  <a href="#" className="block text-center">
+                     Home
+                  </a>
+               </li>
+               <li>
+                  <a href="#" className="block text-center">
+                     About
+                  </a>
+               </li>
+               <li>
+                  <a href="#" className="block text-center">
+                     Service
+                  </a>
+               </li>
+               <li>
+                  <a href="#" className="block text-center">
+                     Contact
+                  </a>
+               </li>
+            </ul>
+         </nav>
+      </>
    )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,41 @@
 export default function Home() {
    return (
       <>
-         <h1 className=" text-3xl">Home</h1>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, debitis?</p>
+         <div className="flex items-center max-w-sm p-6  my-6 bg-white mx-auto shadow-lg  rounded-xl gap-4 dark:bg-slate-800 dark:shadow-none">
+            <img className="size-12" src="/images/chaticon.svg" alt="icon" />
+            <div>
+               <div className="text-xl font-bold text-black dark:text-white">ChaitChat</div>
+               <p className="text-gray-500 dark:text-gray-400">A simple chat application</p>
+            </div>
+         </div>
+
+         <div className="flex flex-col  items-center gap-4 p-6">
+            <button className=" btn btn-primary">Primary</button>
+            <button className=" btn btn-secondary">Secondary</button>
+            <button className="px-4 py-2 cursor-pointer bg-red-500 text-white rounded w-35 hover:bg-red-600 active:bg-red-700">Danger</button>
+            <button className="px-4 py-2 cursor-pointer bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 active:from-green-600 active:to-blue-700 text-white rounded w-35 ">
+               Gradient
+            </button>
+            <button className="px-4 py-2 cursor-pointer  bg-blue-500 text-white rounded w-35 hover:bg-blue-600 active:bg-blue-700 shadow-lg">
+               Shadow
+            </button>
+            <button className="px-4 py-2 cursor-pointer  bg-purple-500 text-white  w-35 hover:bg-purple-600 active:bg-purple-700 shadow-lg rounded-full">
+               Rounded
+            </button>
+            <button
+               className="px-4 py-2  bg-purple-500 text-white  w-35 hover:bg-purple-600 active:bg-purple-700 shadow-lg rounded-full cursor-not-allowed opacity-50"
+               disabled
+            >
+               Disabled
+            </button>
+            <button className="px-4 py-2 cursor-pointer bg-blue-500 text-white rounded w-35 hover:bg-blue-600 active:bg-blue-700 flex items-center gap-2">
+               <img className="size-4" src="/images/chaticon.svg" alt="icon" />
+               Button
+            </button>
+            <button className="px-4 py-2 cursor-pointer bg-blue-500 text-white rounded w-35 hover:bg-blue-600 active:bg-blue-700  active:scale-95 transition-transform">
+               Button
+            </button>
+         </div>
       </>
    )
 }
